6c1346c7669ca06661c80c49a47a2784bfcbdee3
Kevin E. Lee <kevin.ej.lee@gmail.com>
2020-07-08 15:55:57 -0700

    created stateful product-list comp

---
+++ client/components/product-list-item.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export default class ProductListItem extends React.Component {
+
+  // fetch('/api/products')
+  // .then(res => res.json())
+  // .then(data =>{
+  //   console.log(data)
+  // })
+
+  render() {
+    return <h1>Single Product Card</h1>;
+
+  }
+}
---
+++ client/components/product-list.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default class ProductList extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { products: [] };
+  }
+
+}
