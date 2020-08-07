25c7199dab107001077492d8809f9f9c39f88245
Kevin E. Lee <kevin.ej.lee@gmail.com>
2020-08-07 13:36:00 -0700

    created header adjustment to show cart item (barebones)

--- client/components/header.jsx
+++ client/components/header.jsx
@@ -1,10 +1,16 @@
 import React from 'react';
 
 export default class Header extends React.Component {
-
   render() {
     return (
-      <h1 style={{ backgroundColor: '#343A40', color: 'white' }}>$ Wicked Sales</h1>
+      <div style={{ backgroundColor: '#343A40', color: 'white' }}>
+        <div style={{ display: 'flex', margin: '0 auto', justifyContent: 'space-between', alignItems: 'center', width: '95%' }}>
+          <h1 style={{ backgroundColor: '#343A40', color: 'white' }}>
+            $ Wicked Sales
+          </h1>
+          <h4>cartIcon: {this.props.cardItemcount}</h4>
+        </div>
+      </div>
     );
   }
 }
